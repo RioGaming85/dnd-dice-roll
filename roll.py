a1bd22959f7f3729caed682c37ec528e59326ae1
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-09-17 10:48:45 +0100

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,9 +1,12 @@
 def rollDice(diceInp):
   from random import randint
-  from re import findall
+  from re import split as regSplit
   if (diceInp.find('+') != -1): # if there is a modifier
     try:
-      diceNum = findall(r'/([D+])/g', str(diceInp)
+      diceNum=[]
+      diceNum = (regSplit(r'/([D+])/g', str(diceInp)))
+      print(regSplit(r'/([D+])/g', str(diceInp)))
+      print(diceNum)
     except:
       print('Incorrect Syntax.\nThe correct syntax is xDy(+z)')
       return None
@@ -14,7 +17,7 @@ def rollDice(diceInp):
       diceRollArray.append(str(roll))
       diceTotal += roll
     print('Roll ('+str(diceNum[0])+'d'+str(diceNum[1])+'+'+str(diceNum[2])+'):')
-    print(str diceRollArray)+' + '+str(diceNum[2]))
+    print(str(diceRollArray)+' + '+str(diceNum[2]))
 #######################################################################################
   else: # if there is no modifier
     try:
@@ -29,5 +32,5 @@ def rollDice(diceInp):
       diceRollArray.append(str(roll))
       diceTotal += roll
     print('Roll ('+str(diceNum[0])+'d'+str(diceNum[1])+'):')
-    print diceRollArray)
+    print(diceRollArray)
   print('Total: '+ str(diceTotal))
