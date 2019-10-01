9dea8810a2d3ac7d35364b0645570cf0e0aef2e1
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-10-01 10:08:35 +0100

    Update roll.py

--- roll.py
+++ roll.py
@@ -1,6 +1,6 @@
 def rollDice(diceInp):
   from random import randint
-  if (diceInp.find('+') != -1): # if there is a modifier
+  if (diceInp.find('+') != -1): # if there is a positive modifier
     try:
       diceNum = diceInp.split('D')
       splitHolder=(diceNum[1].split('+'))
@@ -8,10 +8,11 @@ def rollDice(diceInp):
       diceNum.append(splitHolder[0])
       diceNum.append(splitHolder[1])
     except:
-      print('Incorrect Syntax.\nThe correct syntax is xDy(+z)')
+      print('Incorrect Syntax.\nThe correct syntax is xDy(modifier)')
       return None
     diceRollArray = []
-    diceTotal = 0
+    diceTotal = diceNum[2]
+    diceTotal = int(diceTotal)
     for x in range(int(diceNum[0])):
       roll = randint(1,int(diceNum[1]))
       diceRollArray.append(str(roll))
@@ -19,6 +20,25 @@ def rollDice(diceInp):
     print('Roll ('+str(diceNum[0])+'d'+str(diceNum[1])+'+'+str(diceNum[2])+'):')
     print(str(diceRollArray)+' + '+str(diceNum[2]))
 #######################################################################################
+  elif (diceInp.find('-') != -1): # if there is a negative modifier
+    try:
+      diceNum = diceInp.split('D')
+      splitHolder=(diceNum[1].split('-'))
+      diceNum.remove(diceNum[1])
+      diceNum.append(splitHolder[0])
+      diceNum.append(splitHolder[1])
+    except:
+      print('Incorrect Syntax.\nThe correct syntax is xDy(modifier)')
+      return None
+    diceRollArray = []
+    diceTotal = int(diceNum[2])
+    for x in range(int(diceNum[0])):
+      roll = randint(1,int(diceNum[1]))
+      diceRollArray.append(str(roll))
+      diceTotal += roll
+    print('Roll ('+str(diceNum[0])+'d'+str(diceNum[1])+'-'+str(diceNum[2])+'):')
+    print(str(diceRollArray)+' + '+str(diceNum[2]))
+  
   else: # if there is no modifier
     try:
       diceNum = diceNum.split('D')  
