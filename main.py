eaaaa9c39032178b46557410646c0d67ee781669
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2019-10-09 09:36:19 +0100

    Add comments

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ CLEAR or CLS: Clear the Output Log
 QUIT or Q: Quit Application
 ROLL or RL: Roll Dice
  > ''').upper()
-    if ans in ['ROLL','RL']:
-      roll(input('\nInput dice and amount:\n > ').upper())
+    if ans in ['ROLL','RL']: # check the input
+      roll(input('\nInput dice and amount:\n > ').upper()) # using the roll function
     elif ans in ['CLEAR','CLS']:
       clear()
     elif ans in ['Q','QUIT']:
@@ -18,5 +18,5 @@ ROLL or RL: Roll Dice
     else:
       print('Invalid Input')
 
-print('\033[1m'+'\nWelcome to Dice Roll!'+'\033[0m')
+print('\033[1m'+'\nWelcome to Dice Roll!'+'\033[0m') # the '\033[1m' is to display the line in bold
 main()
