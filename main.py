069e1d0240f56891b5d5862e3fdfb047e0a73eb8
RioGaming85 <43646340+RioGaming85@users.noreply.github.com>
2020-02-18 12:14:51 +0000

    Changed dice input to in-line command

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from roll import rollDice as roll
 from clear import clear
+from re import split as regSplit
+from re import search
 
 def main():
   while True:
-    ans = input('''
+    ans = input('\n > ').upper()
+    if ans == '':
+      pass
+    else:
+      if search(r'ROLL (\d*)D(\d*) [+] \d',ans) != None or search(r'RL (\d*)D(\d*) [+] \d',ans): # bit of extra string manipulation for modifiers
+        ans = regSplit(r'\s',ans)
+        ans[1] = ans[1] + ans[2] + ans[3]
+        roll(ans[1])
+      elif search(r'ROLL (\d*)D(\d*)',ans) != None or search(r'RL (\d*)D(\d*)',ans) != None: # check the input
+        roll(regSplit(r'\s',ans)[1]) # using the roll function
+      elif ans in ['CLEAR','CLS']:
+        clear()
+        print('''
 CLEAR or CLS: Clear the Output Log
 QUIT or Q: Quit Application
-ROLL or RL: Roll Dice
- > ''').upper()
-    if ans in ['ROLL','RL']: # check the input
-      roll(input('\nInput dice and amount:\n > ').upper()) # using the roll function
-    elif ans in ['CLEAR','CLS']:
-      clear()
-    elif ans in ['Q','QUIT']:
-      print('Goodbye.')
-      break
-    else:
-      print('Invalid Input')
+ROLL or RL: Roll Dice''')
+      elif ans in ['Q','QUIT']:
+        print('Goodbye.')
+        break
+      else:
+        print('Invalid Input')
 
 print('\033[1m'+'\nWelcome to Dice Roll!'+'\033[0m') # the '\033[1m' is to display the line in bold
+print('''
+CLEAR or CLS: Clear the Output Log
+QUIT or Q: Quit Application
+ROLL or RL: Roll Dice''')
 main()
